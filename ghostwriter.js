1d16d7694b24942ef5f1f8b5013f813125cd5ee7
Unknown <lsannar1@mail.csuchico.edu>
2017-04-06 10:12:27 -0700

    got most of it..

--- ghostwriter.js
+++ ghostwriter.js
@@ -5,6 +5,19 @@ var sentence;
 var intro = ["You know what gets me through the day?","Life is full of","Ever heard of","To be happy, one must embrace"];
 var body=["existentialism","a good bowl of wheaties","crippling anxieties","Poorly written Harry Potter Fan-fiction","The Unit Circle"];
 var end=["the end.","and,you know, maybe some other things"];
+randIndex=randomUpTo(intro.length);
+//cant figure out how o assign the value
+sentence=intro[randIndex];
+
+randIndex=randomUpTo(body.length);
+
+sentence=body[randIndex];
+
+randIndex=randomUpTo(end.length);
+
+sentence=end[randIndex];
+
+document.getElementById("tweet").innerHTML=sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
