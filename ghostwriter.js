022728529ab4761002257d17b2624ce390e51e31
Unknown <lsannar1@mail.csuchico.edu>
2017-04-06 09:58:33 -0700

    added intro body and ending

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,10 @@
 function writeTweet()
 {
-  
+var randIndex;
+var sentence;
+var intro = ["You know what gets me through the day?","Life is full of","Ever heard of","To be happy, one must embrace"];
+var body=["existentialism","a good bowl of wheaties","crippling anxieties","Poorly written Harry Potter Fan-fiction","The Unit Circle"];
+var end=["the end.","and,you know, maybe some other things"];
 }
 
 //Generates a random whole number between 0 and max (inclusive)
